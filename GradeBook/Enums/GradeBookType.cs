ac9aef77a51ebdcdb4c8469fc861e90448eac481
ninlau <38708578+ninlau@users.noreply.github.com>
2018-04-25 10:35:03 +0800

    Create a New Enum GradeBookType
    Create a new Enum GradeBookType containing the types "Standard", "Ranked", "ESNU", "OneToFour", and "SixPoint".
    
    This should be located in the Enums directory.
    This should use the GradeBook.Enums namespace.

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standart,
+        Ranked,
+        ESNU,
+        OneToPoint,
+        SixPoint
+    }
+}
