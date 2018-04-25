c57e23255f7d5b2ba16d780a4ae85690e28dac85
ninlau <38708578+ninlau@users.noreply.github.com>
2018-04-25 14:08:27 +0800

    Create StandardGradeBook class
    Create a StandardGradeBook class that inherits the BaseGradeBook class
    
    This should be located in the GradeBooks directory.
    This should use the GradeBook.GradeBooks namespace.
    
    Create a constructor for the StandardGradeBook class that accepts a string parameter called "name". This constructor is required to invoke a constructor from BaseGradeBook. This can be done by adding : base(name) after the constructor declaration. (this will make it so when the StandardGradeBook constructor is called it will execute code in both StandardGradeBook's constructor as well as the BaseGradeBook's constructor)

---
+++ GradeBook/GradeBooks/StandardGradeBooks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBooks : BaseGradeBook 
+    {
+        public StandardGradeBooks(string name) : base(name)
+        {
+            
+
+        }
+    }
+}
