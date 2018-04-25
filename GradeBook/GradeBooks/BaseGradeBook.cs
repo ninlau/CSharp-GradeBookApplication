dccb93389983389082c8ddaf4030ba65a06d1306
ninlau <38708578+ninlau@users.noreply.github.com>
2018-04-25 10:35:35 +0800

    Add Property Type to BaseGradeBook
    In the BaseGradeBook class, add a public property named "Type" of type GradeBookType.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,7 +11,10 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public GradeBookType Type { get; set; }
+
         public string Name { get; set; }
+
         public List<Student> Students { get; set; }
 
         public BaseGradeBook(string name)
